fefb0682e92be7066284cb8b4f8837bdd81fe287
Parryhotter14 <parryhotter14@gmail.com>
2026-02-24 11:44:27 -0300

    Aula 2

---
+++ src/main/java/br/com/senai/clinica/repository/ClinicaRepository.java
@@ -0,0 +1,7 @@
+package br.com.senai.clinica.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClinicaRepository extends JpaRepository<Clinica, Long>{
+    
+}
